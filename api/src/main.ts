e75c0aaf23e05ae6b718ae22e7386f5dcf279c94
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-02-17 19:08:34 -0300

    unificado main

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,33 +6,47 @@ import Routes from "./api/routes/routes";
 import cors from "cors";
 import http, { Server } from "http";
 
-export const startServer = async (): Promise<Server> => {
+const startServer = async (): Promise<Server> => {
   const app: Application = express();
 
+  // Configurações do Express
   app.use(cors(corsOptions));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  // Swagger documentation route
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   try {
+    // Inicializa a conexão com o banco de dados
     await AppDataSource.initialize();
     console.log("Banco de dados conectado.");
 
+    // Inicializa as rotas da API
     new Routes(app);
 
+    // Configuração e inicialização do servidor HTTP
     const PORT: number = 8080;
-    const server: Server = http.createServer(app); // Definindo o servidor HTTP
+    const server: Server = http.createServer(app);
 
+    // Inicia o servidor
     server.listen(PORT, "0.0.0.0", () => {
       console.log(`Servidor rodando na porta ${PORT}.`);
       console.log(`Documentação Swagger disponível em: http://localhost:${PORT}/api-docs`);
     });
 
-    server.unref(); // Isso permite que o processo de testes finalize corretamente
+    // Permite que o processo de testes finalize corretamente
+    server.unref();
+
     return server;
 
   } catch (error) {
     console.error("Erro ao conectar ao banco de dados:", error);
-    throw error; // Re-lançando o erro, para que a falha seja tratada
+    throw error; // Re-lançando o erro para que a falha seja tratada
   }
 };
+
+// Inicia a API ao chamar startServer
+startServer().catch((error) => {
+  console.error("Erro ao iniciar o servidor:", error);
+});
